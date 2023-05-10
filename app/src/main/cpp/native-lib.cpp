1f94b637af974825d4138a4f7d02e073474986b8
mrdcvlsc <mrdcvlsc@gmail.com>
2023-05-10 12:57:55 +0800

    Implement Concurrent Native File Encryption

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -141,11 +141,11 @@ extern "C" JNIEXPORT jintArray JNICALL Java_com_application_bethela_BethelaActiv
 
 /// equivalent to : `this.getApplicationContext().getContentResolver().openInputStream(Uri);`
 jobject openFileUriInputStream (JNIEnv *env, jobject thiz, jobject uri) {
-  return Activity(env, thiz).
-    getApplicationContext().
-    getContentResolver().
-    openInputStream(Uri(env, uri)).
-    _thiz;
+  return Activity(env, thiz)
+    .getApplicationContext()
+    .getContentResolver()
+    .openInputStream(Uri(env, uri))
+    ._thiz;
 }
 
 jstring getFileName (JNIEnv *env, jobject thiz, jobject file_uri) {
@@ -161,14 +161,6 @@ jstring getFileName (JNIEnv *env, jobject thiz, jobject file_uri) {
   jstring fileName = c.getString(c.getColumnIndexOrThrow(DISPLAY_NAME));
   c.close();
   return fileName;
-}
-
-extern "C" JNIEXPORT jstring JNICALL Java_com_application_bethela_BethelaActivity_getFileNameNative (
-  JNIEnv *env,
-  jobject thiz,
-  jobject file_uri
-) {
-  return getFileName(env, thiz, file_uri);
 }
 
 extern "C" JNIEXPORT jint JNICALL Java_com_application_bethela_BethelaActivity_encryptFiles (
@@ -196,7 +188,9 @@ extern "C" JNIEXPORT jint JNICALL Java_com_application_bethela_BethelaActivity_e
 
   JavaVM* javaVM;
   env->GetJavaVM((_JavaVM**) &javaVM);
-  jclass DocumentFileClass = env->FindClass("androidx/documentfile/provider/DocumentFile");
+  jobject DocumentFileClass = env->NewGlobalRef((jobject) env->FindClass("androidx/documentfile/provider/DocumentFile"));
+  jobject globalThis = env->NewGlobalRef(thiz);
+  jobject globalOutputPath = env->NewGlobalRef(output_path);
 
   auto encrypt_lambda = [&] () -> void {
     JNIEnv* threadEnv;
@@ -215,9 +209,9 @@ extern "C" JNIEXPORT jint JNICALL Java_com_application_bethela_BethelaActivity_e
         run_thread = !file_queue.isEmpty(threadEnv);
 
         if (run_thread) {
-          target_uri._thiz = file_queue.remove(file_queue.size() - 1)._thiz;
+          target_uri._thiz = file_queue.remove(threadEnv, file_queue.size(threadEnv) - 1)._thiz;
           target_uri._Jclass = threadEnv->GetObjectClass(target_uri._thiz);
-          jstring filename = getFileName(threadEnv, thiz, target_uri._thiz);
+          jstring filename = getFileName(threadEnv, globalThis, target_uri._thiz);
           const char *c_filename_buffer = threadEnv->GetStringUTFChars(filename, NULL);
           target_file = std::string(c_filename_buffer);
           threadEnv->ReleaseStringUTFChars(filename, c_filename_buffer);
@@ -230,22 +224,27 @@ extern "C" JNIEXPORT jint JNICALL Java_com_application_bethela_BethelaActivity_e
 
         InputStream incoming_bytes = InputStream(
           threadEnv,
-          openFileUriInputStream(threadEnv, thiz, target_uri._thiz)
+          openFileUriInputStream(threadEnv, globalThis, target_uri._thiz)
         );
 
         std::string outfname(target_file + ".bthl");
         jstring jniOutputFileName = threadEnv->NewStringUTF(outfname.c_str());
 
         jstring mimeType = threadEnv->NewStringUTF("application/octet-stream");
+
+        Uri treeUri(threadEnv, globalOutputPath);
+        Context folder_ctx = Activity(threadEnv, globalThis).getApplicationContext();
+
         DocumentFile folder = DocumentFile::fromTreeUri(
           threadEnv,
-          Activity(threadEnv, thiz).getApplicationContext(),
-          Uri(threadEnv, output_path), DocumentFileClass
+          folder_ctx,
+          treeUri,
+          (jclass) DocumentFileClass
         );
 
         DocumentFile outputFile = folder.createFile(mimeType, jniOutputFileName);
 
-        OutputStream outgoing_bytes = Activity(threadEnv, thiz).getApplicationContext().getContentResolver().openOutputStream(outputFile.getUri());
+        OutputStream outgoing_bytes = Activity(threadEnv, globalThis).getApplicationContext().getContentResolver().openOutputStream(outputFile.getUri());
 
         jbyteArray fileSignature = threadEnv->NewByteArray(FILE_SIGNATURE_SIZE);
         const jbyte fileSig[FILE_SIGNATURE_SIZE] = {0x42, 0x45, 0x54, 0x48, 0x45, 0x4c, 0x41};
@@ -272,6 +271,15 @@ extern "C" JNIEXPORT jint JNICALL Java_com_application_bethela_BethelaActivity_e
         jbyteArray jniIV = threadEnv->NewByteArray(AES_BLOCK_SIZE);
         threadEnv->SetByteArrayRegion(jniIV, 0, AES_BLOCK_SIZE, reinterpret_cast<jbyte *>(iv));
         outgoing_bytes.write(jniIV, 0, AES_BLOCK_SIZE);
+
+        jboolean JNIException = threadEnv->ExceptionCheck();
+
+        if (JNIException) {
+          threadEnv->ExceptionDescribe();
+          outgoing_bytes.close();
+          outputFile.Delete();
+          continue;
+        }
 
         while ((length = incoming_bytes.read(jniBuffer)) > 0) {
           jbyte *buffer = (jbyte *) threadEnv->GetPrimitiveArrayCritical(jniBuffer, NULL);
@@ -353,12 +361,12 @@ extern "C" JNIEXPORT jint JNICALL Java_com_application_bethela_BethelaActivity_e
     threads[i].join();
   }
 
+  env->DeleteGlobalRef(DocumentFileClass);
+  env->DeleteGlobalRef(globalThis);
+  env->DeleteGlobalRef(globalOutputPath);
+
   return cnt.load(std::memory_order_relaxed);
 }
-
-
-
-
 
 extern "C" JNIEXPORT jint JNICALL Java_com_application_bethela_BethelaActivity_decryptFiles (
   JNIEnv *env, jobject thiz,
@@ -390,7 +398,6 @@ extern "C" JNIEXPORT jint JNICALL Java_com_application_bethela_BethelaActivity_d
   jobject globalOutputPath = env->NewGlobalRef(output_path);
 
   auto decrypt_lambda = [&] () -> void {
-    __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "start");
     JNIEnv* threadEnv;
     javaVM->AttachCurrentThread((_JNIEnv**) &threadEnv, NULL);
 
@@ -404,10 +411,7 @@ extern "C" JNIEXPORT jint JNICALL Java_com_application_bethela_BethelaActivity_d
         Uri target_uri(threadEnv);
 
         vector_mtx.lock();
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "queue check start");
         run_thread = !file_queue.isEmpty(threadEnv);
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "queue check end");
-
 
         if (run_thread) {
           target_uri._thiz = file_queue.remove(threadEnv, file_queue.size(threadEnv) - 1)._thiz;
@@ -423,13 +427,10 @@ extern "C" JNIEXPORT jint JNICALL Java_com_application_bethela_BethelaActivity_d
 
         vector_mtx.unlock();
 
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "InputStream start");
         InputStream incoming_bytes = InputStream(
           threadEnv,
           openFileUriInputStream(threadEnv, globalThis, target_uri._thiz)
         );
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "InputStream end");
-
 
         std::string outfname(target_file);
         std::string fileExtension = "";
@@ -444,33 +445,19 @@ extern "C" JNIEXPORT jint JNICALL Java_com_application_bethela_BethelaActivity_d
 
         jstring mimeType = threadEnv->NewStringUTF("application/octet-stream");
 
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "Uri treeUri start");
         Uri treeUri(threadEnv, globalOutputPath);
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "Uri treeUri end");
-
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "Context folder_ctx start");
         Context folder_ctx = Activity(threadEnv, globalThis).getApplicationContext();
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "Context folder_ctx end");
 
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "DocumentFile folder start");
         DocumentFile folder = DocumentFile::fromTreeUri(
           threadEnv,
           folder_ctx,
           treeUri,
           (jclass) DocumentFileClass
         );
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "DocumentFile folder end");
 
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "DocumentFile outputFile start");
         DocumentFile outputFile = folder.createFile(mimeType, jniOutputFileName);
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "DocumentFile outputFile end");
-
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "OutputStream outgoing_bytes start");
         OutputStream outgoing_bytes = Activity(threadEnv, globalThis).getApplicationContext().getContentResolver().openOutputStream(outputFile.getUri());
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "OutputStream outgoing_bytes start");
 
-
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "Check Block Start");
         jbyteArray fileSignature = threadEnv->NewByteArray(FILE_SIGNATURE_SIZE);
         incoming_bytes.read(fileSignature);
 
@@ -491,18 +478,16 @@ extern "C" JNIEXPORT jint JNICALL Java_com_application_bethela_BethelaActivity_d
 
         threadEnv->ReleaseByteArrayElements(fileSignature, fileSigRead, 0);
 
-        if (JNIException) {
-          threadEnv->ExceptionDescribe();
-        }
-
         if (signFailed || wrongFileExtension || JNIException) {
+          if (JNIException) {
+            threadEnv->ExceptionDescribe();
+          }
+
           outgoing_bytes.close();
           outputFile.Delete();
           continue;
         }
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "Check Block End");
 
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "Decryption Block Start");
         while ((length = incoming_bytes.read(jniBuffer)) > 0) {
           jbyte *buffer = (jbyte *) threadEnv->GetPrimitiveArrayCritical(jniBuffer, NULL);
 
@@ -549,7 +534,6 @@ extern "C" JNIEXPORT jint JNICALL Java_com_application_bethela_BethelaActivity_d
             outgoing_bytes.write(jniBuffer, 0, recover.length);
           }
         }
-        __android_log_write(ANDROID_LOG_DEBUG, "Native-Thread", "Decryption Block End");
 
         threadEnv->ReleaseByteArrayElements(jniIV, iv, 0);
 
